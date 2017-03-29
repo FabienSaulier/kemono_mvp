9427793359cdfb1ccc0c2239d7389c7c20fb9d22
Fabien Saulier <fsaulier@gmail.com>
2017-03-29 03:42:45 +0200

    datepicker

--- imports/ui/profil/EditProfil.js
+++ imports/ui/profil/EditProfil.js
@@ -1,27 +1,48 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Button, Header, Form} from 'semantic-ui-react'
-//import {updateUser} from '../../api/users/methods'
+import DatePicker from 'react-datepicker'
+import moment from 'moment'
+
+import 'react-datepicker/dist/react-datepicker.css';
 
  export class EditProfil extends React.Component {
   constructor(props) {
     console.log(props);
     super(props);
+
+    let userProfil = props.currentUser.profile;
     this.state = {
+
+      'birthday': moment().locale('fr'),
+      'picture': userProfil.picture,
+      'phone': userProfil.phone,
+      'userProfile': '222',
+      'test': {'a':'b','c':'d'}
+
       //TODO init
     };
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleDateChange = this.handleDateChange.bind(this);
     this.save = this.save.bind(this);
   }
 
   handleInputChange(event) {
+    console.log(event);
     const target = event.target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
     const name = target.name;
+    console.log(this.state);
     this.setState({
       [name]: value
     });
   }
+
+  handleDateChange(event){
+    console.log(event);
+    this.setState({'birthday': event});
+  }
+
 
   save(event){
     event.preventDefault();
@@ -41,44 +62,41 @@ import { Button, Header, Form} from 'semantic-ui-react'
 
   render() {
     let user = this.props.currentUser.profile;
-
     return (
       <div>
         <Header size='large'>Mon profil</Header>
-
-
         Edit Profil: {user.firstName} {user.lastName}
-
         <Form size='huge'>
-          <Form.Field inline required>
+          <Form.Field inline required  >
             <label>birthday</label>
-            <input placeholder='birthday' name='birthday' value={user.birthday} onChange={this.handleInputChange}/>
+            <DatePicker onChange={this.handleDateChange} selected={this.state.birthday} name="birthday"
+               peekNextMonth showMonthDropdown showYearDropdown dropdownMode="select" placeholderText="Birthday" />
           </Form.Field>
-          <Form.Field inline>
+          <Form.Field inline  >
             <label>picture</label>
-            <input placeholder='picture' name='picture' value={user.picture} onChange={this.handleInputChange}/>
+            <input placeholder='picture' required  name='picture' value={this.state.picture} onChange={this.handleInputChange}/>
           </Form.Field>
-          <Form.Field inline>
+          <Form.Field inline >
             <label>phone</label>
-            <input placeholder='phone' name='phone' value={user.phone} onChange={this.handleInputChange}/>
+            <input placeholder='phone'    name='phone' value={this.state.phone} onChange={this.handleInputChange}/>
           </Form.Field>
-          <Form.Field inline>
+          <Form.Field inline >
             <label>address.street</label>
-            <input placeholder='addressStreet' name='addressStreet' onChange={this.handleInputChange}/>
+            <input placeholder='addressStreet'  name='addressStreet' onChange={this.handleInputChange}/>
           </Form.Field>
-          <Form.Field inline>
+          <Form.Field inline >
             <label>address.city</label>
-            <input placeholder='addressCity' name='addressCity'  onChange={this.handleInputChange} />
+            <input placeholder='addressCity'  name='addressCity'  onChange={this.handleInputChange} />
           </Form.Field>
-          <Form.Field inline>
+          <Form.Field inline >
             <label>address.country</label>
-            <input placeholder='addressCountry' name='addressCountry' onChange={this.handleInputChange}/>
+            <input placeholder='addressCountry'     name='addressCountry' onChange={this.handleInputChange}/>
           </Form.Field>
-          <Form.Field inline>
+          <Form.Field inline  >
             <label>address.zip</label>
-            <input placeholder='addressZip' name='addressZip' onChange={this.handleInputChange} />
+            <input placeholder='addressZip'  name='addressZip' onChange={this.handleInputChange} />
           </Form.Field>
-          <Button type='submit' content='Enregistrer' onClick={this.save} icon='save' size='big' labelPosition='left' >Submit</Button>
+          <Button type='submit' onClick={this.save}  size='big' labelPosition='left' >Enregistrer</Button>
         </Form>
       </div>
     )
