2bfdb90f6db8f71da5bdf03d7b80cc7896e9fa59
Fabien Saulier <fsaulier@gmail.com>
2017-04-03 02:49:28 +0200

    fake data pr visu

--- imports/ui/pages/Index.js
+++ imports/ui/pages/Index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Jumbotron } from 'react-bootstrap';
 import { Segment, Label} from 'semantic-ui-react'
+import { Card, Icon, Image } from 'semantic-ui-react'
 
 import PetsDashboardComponent from '../components/PetsDashboardComponent'
 
@@ -12,10 +13,7 @@ export const Index = ({user}) => (
   <div className="Index">
     <Segment padded='very' raised>
       <Label attached='top left' style={{'fontSize':'14px'}}>En cours</Label>
-      Le coût des soins vétérinaires peut être très élevé.
-      Découvrez comment Kemono aide les propriétaires à faire face ensemble,
-      pour le bien-être de leurs compagnons ! Intégrez le réseau de partenaires Kemono pour accroître votre patientèle et votre visibilité sur Internet.
-      Pellentesque habitant morbi tristique senectus.
+      Vous n'avez pas d'opération en cours.
     </Segment>
     <Segment padded='very' raised>
       <Label attached='top left' style={{'fontSize':'14px'}}>Mes compagnons</Label>
@@ -23,7 +21,53 @@ export const Index = ({user}) => (
     </Segment>
     <Segment padded='very'>
       <Label attached='top left' style={{'fontSize':'14px'}}>Les campagnes en cours sur Kemono</Label>
-      Pellentesque habitant morbi tristique senectus.
+
+
+<Card.Group>
+  <Card>
+    <Card.Content>
+      <Image floated='left' size='small' src='/img/chat1.jpg' />
+      <Card.Header>
+        Minou
+      </Card.Header>
+      <Card.Meta>
+        Le chat de Delphine
+      </Card.Meta>
+      <Card.Description>
+        Il s'est cassé la patte en sautant du balcon. J'habite au 3° étage, c'est un peu haut, aieaie, il souffre :'
+      </Card.Description>
+    </Card.Content>
+  </Card>
+  <Card>
+    <Card.Content>
+      <Image floated='right' size='medium' src='/img/chien1.jpg' />
+      <Card.Header>
+        Filou
+      </Card.Header>
+      <Card.Meta>
+        Le chien de Max
+      </Card.Meta>
+      <Card.Description>
+        Faut-il vraiment mettre une description?
+      </Card.Description>
+    </Card.Content>
+  </Card>
+  <Card>
+    <Card.Content>
+      <Image floated='right' size='small' src='/img/chien2.jpg' />
+      <Card.Header>
+        Jenny
+      </Card.Header>
+      <Card.Meta>
+        Le chien de Michel
+      </Card.Meta>
+      <Card.Description>
+        Blablablab blablab Blablabla Blablablab Blablablab Blablablab Blablablab
+      </Card.Description>
+    </Card.Content>
+  </Card>
+</Card.Group>
+
     </Segment>
   </div>
 ) : (
