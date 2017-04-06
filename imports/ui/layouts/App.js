3e97ea0937367fb3d62527cfe7f2dd335fff499b
Fabien Saulier <fsaulier@gmail.com>
2017-04-06 13:40:02 +0200

    footer

--- imports/ui/layouts/App.js
+++ imports/ui/layouts/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid } from 'react-bootstrap';
+import { Row, Col, Grid } from 'react-bootstrap';
 
 import AppNavigationContainer from '../containers/AppNavigationContainer.js';
 //import InsiderMenu from '../components/InsiderMenu.js';
@@ -15,6 +15,24 @@ const App = ({ children }) => (
 
       { children }
     </Grid>
+    <footer>
+      <div className="container-fluid" id="container_footer">
+        <Row  align="center">
+          <Col col-xs-12 col-sm-12 col-md-12 col-lg-12>
+            <div className="social_foot">
+              <ul>
+                <li><a href="#" target="_blank"><img src="/img/facebook-logo-button_white.png" className="img-responsive img-footer"/></a></li>
+                <li><a href="#" target="_blank"><img src="/img/twitter-logo-button_white.png" className="img-responsive img-footer"/></a></li>
+                <li><a href="#" target="_blank"><img src="/img/google-plus-logo-button_white.png" className="img-responsive img-footer"/></a></li>
+                <li><a href="#" target="_blank"><img src="/img/linkedin-logo-button_white.png" className="img-responsive img-footer"/></a></li>
+                <li><a href="#" target="_blank"><img src="/img/instagram-logo_white.png" className="img-responsive img-footer"/></a></li>
+              </ul>
+            </div>
+          </Col>
+          <p className="footnotes">© 2017 Kemono <br/></p>
+        </Row>
+      </div>
+    </footer>
   </div>
 );
 
