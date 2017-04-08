af9fbbbac5294e02f6b11a53ad4f8a71a036c7ff
Fabien Saulier <fsaulier@gmail.com>
2017-04-08 20:25:18 +0200

    date human

--- imports/ui/pages/RecoverPassword.js
+++ imports/ui/pages/RecoverPassword.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row, Col, Alert, FormGroup, FormControl, Button } from 'react-bootstrap';
+import { Row, Col, Alert, FormGroup, FormControl, Button, Panel } from 'react-bootstrap';
 import handleRecoverPassword from '../../modules/recover-password';
 
 export default class RecoverPassword extends React.Component {
@@ -16,25 +16,26 @@ export default class RecoverPassword extends React.Component {
       <div className="RecoverPassword">
         <Row>
           <Col xs={ 12 } sm={ 6 } md={ 4 }>
-            <h4 className="page-header">Recover Password</h4>
-            <Alert bsStyle="info">
-              Enter your email address below to receive a link to reset your password.
-            </Alert>
-            <form
-              ref={ form => (this.recoverPasswordForm = form) }
-              className="recover-password"
-              onSubmit={ this.handleSubmit }
-            >
-              <FormGroup>
-                <FormControl
-                  type="email"
-                  ref="emailAddress"
-                  name="emailAddress"
-                  placeholder="Email Address"
-                />
-              </FormGroup>
-              <Button type="submit" bsStyle="success">Recover Password</Button>
-            </form>
+            <Panel header="Récupérer votre mot de passe">
+              <Alert bsStyle="info">
+                Entrer votre adresse mail pour recevoir un lien et choisir un nouveau mot de passe.
+              </Alert>
+              <form
+                ref={ form => (this.recoverPasswordForm = form) }
+                className="recover-password"
+                onSubmit={ this.handleSubmit }
+              >
+                <FormGroup>
+                  <FormControl
+                    type="email"
+                    ref="emailAddress"
+                    name="emailAddress"
+                    placeholder="Email Address"
+                  />
+                </FormGroup>
+                <Button type="submit" bsStyle="success">Récupérer votre mot de passe</Button>
+              </form>
+            </Panel>
           </Col>
         </Row>
       </div>
