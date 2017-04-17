343f64fccca6a9cb3ff72db0af7d881f1e675160
Fabien Saulier <fsaulier@gmail.com>
2017-04-17 20:47:20 +0200

    add proptype check for imagesProfil

--- imports/ui/components/ProfilImage.js
+++ imports/ui/components/ProfilImage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {Image} from 'react-bootstrap';
 
 const ProfilImage = (props) => {
@@ -10,6 +11,11 @@ const ProfilImage = (props) => {
     else
       return <Image responsive rounded src='/img/no_pic_cat.jpg' />
   }
+}
+
+ProfilImage.propTypes = {
+  idImage: PropTypes.string,
+  type: PropTypes.string
 }
 
 export default ProfilImage;
