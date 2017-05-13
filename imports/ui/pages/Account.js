654f94fc5e39e706ed4bbcd8657fe3e2fba22d12
Fabien Saulier <fsaulier@gmail.com>
2017-05-13 13:17:09 +0200

    display historique paiement

--- imports/ui/pages/Account.js
+++ imports/ui/pages/Account.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Table } from 'react-bootstrap';
+import moment from 'moment';
 
 export class Account extends React.Component {
   constructor(props) {
@@ -9,8 +11,36 @@ export class Account extends React.Component {
   render() {
     return(
       <div>
-        Print la list des transactions
+        <h3>Historique des paiements</h3>
+        <Table >
+          <thead>
+            <tr>
+              <th>N° de transaction</th>
+              <th>Débité</th>
+              <th>Date</th>
+              <th>Statut</th>
+            </tr>
+          </thead>
+          <tbody>
+            {renderTransactionRow(this.props.transactionsList)}
+          </tbody>
+        </Table>
       </div>
     )
   }
 };
+
+const renderTransactionRow = (transactionsList) => {
+  if(!transactionsList) return null;
+
+  return transactionsList.map(function(transaction,i){
+    return (
+      <tr  key={i}>
+        <td>{transaction.Id}</td>
+        <td>{transaction.DebitedFunds.Amount/100} €</td>
+        <td>{moment.unix(transaction.ExecutionDate).format('DD - MM - YYYY HH:mm')}</td>
+        <td>{transaction.Status}</td>
+      </tr>
+    )
+  })
+}
